56e4aaad6f72d321a1adb4491327dddebc4b17b3
Quan <duyquan071299@gmail.com>
2020-07-20 02:50:12 +0700

    update

--- src/models/company.js
+++ src/models/company.js
@@ -5,140 +5,135 @@ import { getCompany, updateCompany, getCompanyById, fullCreateCompany, quickCrea
 import { getContact } from '../services/contact';
 
 const Model = {
-  namespace: 'company',
-  state: {
-    companyInfo: undefined,
-    contactInfo: [],
-    searchValueContact: [],
-    data: undefined,
-    itemCount: undefined,
-    searchCompanyValue: '',
-  },
-  effects: {
-    *fullCreate({ payload }, { call }) {
-      yield call(fullCreateCompany, payload);
-
-      message.success('Tạo Company thành công');
-      history.push({
-        pathname: '/company/',
-      });
+    namespace: 'company',
+    state: {
+        companyInfo: undefined,
+        contactInfo: [],
+        searchValueContact: [],
+        data: undefined,
+        itemCount: undefined,
+        searchCompanyValue: '',
     },
-    *quickCreateCompany({ payload }, { call }) {
-      const createdCompany = yield call(quickCreateCompanyServices, payload);
-      if (createdCompany.id) {
-        const value = {
-          value: createdCompany.id.toString(),
-          label: createdCompany.name,
-          key: createdCompany.id.toString()
-        }
-        return value;
-      }
-    },
-    *searchContactByName({ payload }, { call, put }) {
-      if (payload.value === '') return;
-      const response = yield call(getContact, payload);
-      const formatedData = [];
+    effects: {
+        * fullCreate({ payload }, { call }) {
+            yield call(fullCreateCompany, payload);
 
-      response.data.forEach((element) => {
-        const data = {
-          key: element.id.toString(),
-          label: element.name,
-          value: element.id.toString(),
-        };
-        formatedData.push(data);
-      });
-
-      if (response != null) {
-        yield put({
-          type: 'saveListContact',
-          payload: formatedData,
-        });
-      }
-    },
-    *searchCompanyByName(
-      {
-        payload = {
-          page: 1,
-          searchValue: '',
+            message.success('Tạo Company thành công');
+            history.push({
+                pathname: '/company/',
+            });
         },
-      },
-      { call, put },
-    ) {
-      yield put({
-        type: 'saveCompanySearchValue',
-        payload: payload.searchValue,
-      });
-
-      const response = yield call(getCompany, payload);
-
-      if (response != null) {
-        yield put({
-          type: 'saveCompanyInfo',
-          payload: formatedListCompanyData(response),
-        });
-      }
-    },
-    *loadListCompany(
-      {
-        payload = {
-          page: 1,
-          searchValue: '',
+        * quickCreateCompany({ payload }, { call }) {
+            const createdCompany = yield call(quickCreateCompanyServices, payload);
+            if (createdCompany.id) {
+                const value = {
+                    value: createdCompany.id.toString(),
+                    label: createdCompany.name,
+                    key: createdCompany.id.toString()
+                }
+                return value;
+            }
+            return null;
         },
-      },
-      { call, put },
-    ) {
-      const response = yield call(getCompany, payload);
-      if (response != null) {
-        yield put({
-          type: 'saveCompanyInfo',
-          payload: formatedListCompanyData(response),
-        });
-      }
-    },
-    *update({ payload }, { call }) {
-      yield call(updateCompany, payload);
+        * searchContactByName({ payload }, { call, put }) {
+            if (payload.value === '') return;
+            const response = yield call(getContact, payload);
+            const formatedData = [];
 
-      history.push({
-        pathname: '/company',
-      });
-      message.success('Cập nhật Company thành công');
-    },
-    *loading({ payload }, { call, put }) {
-      const response = yield call(getCompanyById, payload);
+            response.data.forEach((element) => {
+                const data = {
+                    key: element.id.toString(),
+                    label: element.name,
+                    value: element.id.toString(),
+                };
+                formatedData.push(data);
+            });
 
-      yield put({
-        type: 'loadCompany',
-        payload: formatedDetailCompanyData(response),
-      });
+            if (response != null) {
+                yield put({
+                    type: 'saveListContact',
+                    payload: formatedData,
+                });
+            }
+        },
+        * searchCompanyByName({
+            payload = {
+                page: 1,
+                searchValue: '',
+            },
+        }, { call, put }, ) {
+            yield put({
+                type: 'saveCompanySearchValue',
+                payload: payload.searchValue,
+            });
+
+            const response = yield call(getCompany, payload);
+
+            if (response != null) {
+                yield put({
+                    type: 'saveCompanyInfo',
+                    payload: formatedListCompanyData(response),
+                });
+            }
+        },
+        * loadListCompany({
+            payload = {
+                page: 1,
+                searchValue: '',
+            },
+        }, { call, put }, ) {
+            const response = yield call(getCompany, payload);
+            if (response != null) {
+                yield put({
+                    type: 'saveCompanyInfo',
+                    payload: formatedListCompanyData(response),
+                });
+            }
+        },
+        * update({ payload }, { call }) {
+            yield call(updateCompany, payload);
+
+            history.push({
+                pathname: '/company',
+            });
+            message.success('Cập nhật Company thành công');
+        },
+        * loading({ payload }, { call, put }) {
+            const response = yield call(getCompanyById, payload);
+
+            yield put({
+                type: 'loadCompany',
+                payload: formatedDetailCompanyData(response),
+            });
+        },
     },
-  },
-  reducers: {
-    saveCompanyInfo(state, { payload }) {
-      return { ...state, companyInfo: payload.data, itemCount: payload.itemCount };
+    reducers: {
+        saveCompanyInfo(state, { payload }) {
+            return {...state, companyInfo: payload.data, itemCount: payload.itemCount };
+        },
+        handleCreateModal(state, { payload }) {
+            return {...state, visible: payload };
+        },
+        loadCompany(state, { payload }) {
+            return {...state, data: payload };
+        },
+        cleanData(state) {
+            return {...state, data: undefined, searchValueContact: [] };
+        },
+        handleSearchContactChange(state, { payload }) {
+            return {
+                ...state,
+                searchValueContact: payload.value,
+                contactInfo: payload.contactInfo,
+            };
+        },
+        saveCompanySearchValue(state, { payload }) {
+            return {...state, searchCompanyValue: payload };
+        },
+        saveListContact(state, { payload }) {
+            return {...state, contactInfo: payload };
+        },
     },
-    handleCreateModal(state, { payload }) {
-      return { ...state, visible: payload };
-    },
-    loadCompany(state, { payload }) {
-      return { ...state, data: payload };
-    },
-    cleanData(state) {
-      return { ...state, data: undefined, searchValueContact: [] };
-    },
-    handleSearchContactChange(state, { payload }) {
-      return {
-        ...state,
-        searchValueContact: payload.value,
-        contactInfo: payload.contactInfo,
-      };
-    },
-    saveCompanySearchValue(state, { payload }) {
-      return { ...state, searchCompanyValue: payload };
-    },
-    saveListContact(state, { payload }) {
-      return { ...state, contactInfo: payload };
-    },
-  },
 };
 
 export default Model;
--- src/models/task.js
+++ src/models/task.js
@@ -2,114 +2,105 @@ import { message } from 'antd';
 import { createTaskTouchpoint, updateTaskTouchpoint } from '../services/touchpoint';
 
 const User = {
-  'chau.dh': '48862ade-6f9a-471f-835a-cff4f3b9a567',
-  'tu.tt': '50b0cb2e-3782-4b11-82c0-4e2f6580ab94',
-  'khoa.nd': '171ecb82-4daa-43dc-8fec-61878b42d506',
-  'nhan.lh': '39d088f6-cc81-4263-ac27-b920983a4eb0',
+    'chau.dh': '48862ade-6f9a-471f-835a-cff4f3b9a567',
+    'tu.tt': '50b0cb2e-3782-4b11-82c0-4e2f6580ab94',
+    'khoa.nd': '171ecb82-4daa-43dc-8fec-61878b42d506',
+    'nhan.lh': '39d088f6-cc81-4263-ac27-b920983a4eb0',
 };
 
 const Model = {
-  namespace: 'task',
-  state: {
-    listTask: [],
-  },
-  effects: {
-    *create({ payload }, { call, put }) {
-      const response = yield call(createTaskTouchpoint, payload);
-
-      if (response != null) {
-        const newValue = {
-          id: response.id,
-          touchpointId: payload.touchpointId,
-          taskname: response.taskname,
-          type: response.type,
-          userId: response.userId,
-          dueDate: response.dueDate,
-        };
-        yield put({
-          type: 'addTask',
-          payload: newValue,
-        });
-      }
-
-      message.success('Successfully');
-    },
-    *update({ payload }, { call, put }) {
-      console.table(payload);
-      const newData = {
-        id: payload.listTask[payload.index].id,
-        touchpointId: payload.touchpointId,
-        taskname: payload.newData.taskname,
-        type: payload.newData.type,
-        userId: User[payload.newData.pic],
-        duedate: payload.newData.duedate.format('YYYY-MM-DD HH:mm'),
-      };
-      const response = yield call(updateTaskTouchpoint, newData);
-      console.table(response);
-      const updateData = {
-        id: response.id,
-        touchpointId: payload.listTask[payload.index].touchpointId,
-        taskname: payload.newData.taskname,
-        type: payload.newData.type,
-        userId: User[payload.newData.pic],
-        duedate: payload.newData.duedate.format('YYYY-MM-DD HH:mm'),
-      };
-      yield put({
-        type: 'updateTask',
-        payload: {
-          updateData,
-          oldId: payload.listTask[payload.index].id,
-        },
-      });
-    },
-  },
-
-  reducers: {
-    saveListTask(state, { payload }) {
-      return {
-        ...state,
-        listTask: payload,
-      };
-    },
-    addTask(state, { payload }) {
-      const newList = state.listTask;
-      newList.push(payload);
-      console.table(newList);
-      return {
-        ...state,
-        listTask: newList,
-      };
-    },
-    updateTask(state, { payload }) {
-      console.table(payload);
-      const currentList = state.listTask;
-      console.table(currentList);
-      const index = currentList.findIndex((item) => payload.oldId === item.id);
-      console.table(index);
-      const selectItem = currentList[index];
-      console.table(selectItem);
-      currentList.splice(index, 1, { ...selectItem, ...payload.updateData });
-      console.table(currentList);
-      return {
-        ...state,
-        listTask: currentList,
-      };
-    },
-    cleanData(state) {
-      return {
-        ...state,
+    namespace: 'task',
+    state: {
         listTask: [],
-      };
     },
-    removeTask(state, { payload }) {
-      const newList = state.listTask;
-      newList.push(payload);
-      return {
-        ...state,
-        listTask: newList,
-      };
+    effects: {
+        * create({ payload }, { call, put }) {
+            const response = yield call(createTaskTouchpoint, payload);
+
+            if (response != null) {
+                const newValue = {
+                    id: response.id,
+                    touchpointId: payload.touchpointId,
+                    taskname: response.taskname,
+                    type: response.type,
+                    userId: response.userId,
+                    dueDate: response.dueDate,
+                };
+                yield put({
+                    type: 'addTask',
+                    payload: newValue,
+                });
+            }
+
+            message.success('Successfully');
+        },
+        * update({ payload }, { call, put }) {
+            const newData = {
+                id: payload.listTask[payload.index].id,
+                touchpointId: payload.touchpointId,
+                taskname: payload.newData.taskname,
+                type: payload.newData.type,
+                userId: User[payload.newData.pic],
+                duedate: payload.newData.duedate.format('YYYY-MM-DD HH:mm'),
+            };
+            const response = yield call(updateTaskTouchpoint, newData);
+            const updateData = {
+                id: response.id,
+                touchpointId: payload.touchpointId,
+                taskname: payload.newData.taskname,
+                type: payload.newData.type,
+                userId: User[payload.newData.pic],
+                duedate: payload.newData.duedate.format('YYYY-MM-DD HH:mm'),
+            };
+            yield put({
+                type: 'updateTask',
+                payload: {
+                    updateData,
+                    index: payload.index,
+                },
+            });
+        },
     },
-  },
+
+    reducers: {
+        saveListTask(state, { payload }) {
+            return {
+                ...state,
+                listTask: payload,
+            };
+        },
+        addTask(state, { payload }) {
+            const newList = state.listTask;
+            newList.push(payload);
+            return {
+                ...state,
+                listTask: newList,
+            };
+        },
+        updateTask(state, { payload }) {
+            const currentList = state.listTask;
+            const selectItem = currentList[payload.index];
+            currentList.splice(payload.index, 1, {...selectItem, ...payload.updateData });
+            return {
+                ...state,
+                listTask: currentList,
+            };
+        },
+        cleanData(state) {
+            return {
+                ...state,
+                listTask: [],
+            };
+        },
+        removeTask(state, { payload }) {
+            const newList = state.listTask;
+            newList.push(payload);
+            return {
+                ...state,
+                listTask: newList,
+            };
+        },
+    },
 };
 
 export default Model;
