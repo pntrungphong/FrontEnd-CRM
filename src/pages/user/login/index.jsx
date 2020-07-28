799235e68849fdf5e742acf9c60417dde2f8c1cf
hcminh <hoangminhh17@gmail.com>
2020-07-29 00:08:45 +0700

    update

--- src/pages/user/login/index.jsx
+++ src/pages/user/login/index.jsx
@@ -1,8 +1,7 @@
-// import { LockOutlined, UserOutlined } from '@ant-design/icons';
 import { Form, Input, Button, Checkbox } from 'antd';
 import React from 'react';
 import { connect } from 'umi';
-import styles from './index.less';
+import styles from './style.less';
 
 const NormalLoginForm = (props) => {
   const onFinish = (values) => {
@@ -13,77 +12,76 @@ const NormalLoginForm = (props) => {
     });
   };
   return (
-    <div className={styles.small}>
-      <Form
-        name="normal_login"
-        className={styles.cusForm}
-        initialValues={{
-          remember: true,
-        }}
-        onFinish={onFinish}
-      >
-        <div>
-          <img src="/logo.png" alt="" width="500px" height="172px" />
-        </div>
-        {/* <h1 className={styles.titleOne}>Welcome</h1> */}
-
-        <div className={styles.mediumOne}>
-          <Form.Item
-            name="email"
-            rules={[
-              {
-                required: true,
-                message: 'Please input your Email!',
-              },
-            ]}
+    <div className={styles.boxContainer}>
+      <div className={styles.boxContent}>
+        <div className={styles.formBox}>
+          <Form
+            name="normal_login"
+            className={styles.customForm}
+            layout="vertical"
+            initialValues={{
+              remember: true,
+            }}
+            onFinish={onFinish}
           >
-            {/* <h3>Email</h3> */}
-            <Input
-              // prefix={<UserOutlined className="site-form-item-icon" />}
-              placeholder="Email"
-              className={styles.inputOne}
-            />
-          </Form.Item>
+            <div className={styles.loginLogo}>
+              <img src="/square-logo.png" alt="" />
+            </div>
+            <h1 className={styles.loginTitle}>Welcome!</h1>
 
-          <Form.Item
-            name="password"
-            rules={[
-              {
-                required: true,
-                message: 'Please input your Password!',
-              },
-            ]}
-          >
-            {/* <h3>PassWord:</h3> */}
-            <Input
-              className={styles.inputOne}
-              // prefix={<LockOutlined className={styles.icon} />}
-              type="password"
-              placeholder="Password"
-            />
-          </Form.Item>
-          <div className={styles.demo}>
-            <Checkbox>Remember Me</Checkbox>
-            <a>
-              <b className={styles.text}>Forgot Password</b>
-            </a>
-          </div>
+            <div className={styles.loginForm}>
+              <Form.Item
+                className={styles.customFormLabel}
+                name="email"
+                label="Email"
+                rules={[
+                  {
+                    required: true,
+                    message: 'Please input your Email!',
+                  },
+                ]}
+              >
+                <Input size="large" placeholder="Email" className={styles.inputOne} />
+              </Form.Item>
 
-          <Form.Item>
-            <Button
-              loading={props.submitting}
-              type="primary"
-              htmlType="submit"
-              className={styles.butt}
-            >
-              Log in
-            </Button>
-          </Form.Item>
+              <Form.Item
+                name="password"
+                label="Password"
+                rules={[
+                  {
+                    required: true,
+                    message: 'Please input your Password!',
+                  },
+                ]}
+              >
+                <Input
+                  className={styles.inputOne}
+                  size="large"
+                  type="password"
+                  placeholder="Password"
+                />
+              </Form.Item>
+              <div className={styles.subtitle}>
+                <Checkbox>Remember Me</Checkbox>
+                <a>
+                  <b className={styles.text}>Forgot Password</b>
+                </a>
+              </div>
+
+              <Form.Item>
+                <Button
+                  block
+                  loading={props.submitting}
+                  type="primary"
+                  htmlType="submit"
+                  className={styles.loginButton}
+                >
+                  Sign in
+                </Button>
+              </Form.Item>
+            </div>
+          </Form>
         </div>
-      </Form>
-
-      <div className={styles.imgMain}>
-        <img src="https://geekup.hexaspace.vn/images/254e00fa6bec87d593748b68dfe69313.svg" alt="" />
       </div>
     </div>
   );
