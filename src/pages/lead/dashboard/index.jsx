4a46a54f3e6afe0c338652ecc20154bc98af1f14
minh.hc <minh.hc@geekup.vn>
2020-08-07 14:37:57 +0700

    xoa source

--- src/pages/lead/dashboard/index.jsx
+++ src/pages/lead/dashboard/index.jsx
@@ -9,7 +9,7 @@ import TouchPointCreateForm from './components/leadDetailModal/leadModal';
 
 const DEFAULTLISTTP = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 class DashBoard extends React.Component {
-  componentWillMount() {
+  UNSAFE_componentWillMount() {
     this.props.dispatch({ type: 'lead/getListWithLane', payload: {} });
   }
 
@@ -37,16 +37,13 @@ class DashBoard extends React.Component {
                       key={LISTDEFAULTLANE[index]}
                       align="center"
                       direction="horizontal"
-                      className={styles.customSpaceTP}
-                    >
+                      className={styles.customSpaceTP}>
                       {DEFAULTLISTTP.map((_, tpIndex) => {
                         return (
-                          <div className={styles.boxStateTP} key={LISTDEFAULTLANE[index] + tpIndex}>
+                          <div className={styles.boxStateTP} key={`${LISTDEFAULTLANE[index]}${tpIndex}`}>
                             {index === 0 ? (
-                              <h1 className={styles.touchPointTitle}>#{tpIndex || 'Bus stop'}</h1>
-                            ) : (
-                              ''
-                            )}
+                              <h1 className={styles.touchPointTitle}>#{tpIndex}</h1>
+                            ) : ('')}
                             <Card className={styles.touchPointBox}>
                               {listLead.map((leadItem) => {
                                 if (leadItem.touchPoint[0]?.order === tpIndex)
@@ -57,8 +54,7 @@ class DashBoard extends React.Component {
                                       currentTouchPoint={leadItem.touchPoint[0]}
                                       leadDetail={leadItem}
                                       show={false}
-                                      key={`TPUpt${leadItem.touchPoint[0]?.id}`}
-                                    >
+                                      key={`TPUpt${leadItem.touchPoint[0]?.id}`}>
                                       <CardLead key={leadItem.touchPoint[0]?.id} lead={leadItem} />
                                     </TouchPointCreateForm>
                                   );
